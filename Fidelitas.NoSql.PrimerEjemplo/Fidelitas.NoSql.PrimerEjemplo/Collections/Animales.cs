37839aeac51d3dcef912e70338c8a5fcb46fa9b6
Laboratorios <Laboratorios@Q26>
2019-02-21 21:08:55 -0600

    solución de animales

---
+++ Fidelitas.NoSql.PrimerEjemplo/Fidelitas.NoSql.PrimerEjemplo/Collections/Animales.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fidelitas.NoSql.PrimerEjemplo.Collections
+{
+    /*
+    "_id" : 1,
+    "nombre" : "firulais",
+    "fecha" : ISODate("2019-01-01T00:00:00.000Z"),
+    "tipo" : "perro",
+    "dueno" : "pepito",
+    "tratamiento" : [
+        {
+            "tipo" : "operación",
+            "fecha" : ISODate("2019-02-01T00:00:00.000Z"),
+            "precio" : 500
+
+      */
+    public class Animales
+    {
+        ObjectId _id;
+        string Nombre;
+        DateTime fecha;
+        string Tipo;
+        string Dueno;
+        IList <Tratamiento> tratamiento;
+    }
+    public class Tratamiento
+    {
+        string Tipo;
+        DateTime Fecha;
+        decimal precio;
+    }
+}
---
+++ Fidelitas.NoSql.PrimerEjemplo/Fidelitas.NoSql.PrimerEjemplo/Controllers/AnimalController.cs
@@ -0,0 +1,103 @@
+﻿using Fidelitas.NoSql.PrimerEjemplo.Collections;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fidelitas.NoSql.PrimerEjemplo.Controllers
+{
+    public class AnimalController : Controller
+    {
+        public AnimalController()
+        {
+        }
+
+
+        // GET: Animal
+        public ActionResult Index()
+        {
+            var connectionString = ConfigurationManager.AppSettings.Get("mongoConnection");
+            var client = new MongoClient (connectionString);
+            var database = client.GetDatabase(ConfigurationManager.AppSettings.Get("databaseName"));
+            var animales = database.GetCollection<Animales>("animales");
+            var res = database.RunCommand<BsonDocument>(BsonDocument.Parse("{ dbStats: 1, scale: 1024 }"));
+            return Json(res, JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: Animal/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Animal/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Animal/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Animal/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Animal/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Animal/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Animal/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
